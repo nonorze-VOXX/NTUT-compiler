d7386dd806ab60274ba5767ffb27661c930a696a
nonorze-VOXX <appieosugood@gmail.com>
2024-10-03 07:53:11 -0400

    smol fix

--- hw1/ex4.s
+++ hw1/ex4.s
@@ -2,14 +2,17 @@
 .format: .string "%d\n"
 .false: .string "false\n"
 .true: .string "true\n"
+x: .int 2
+y: .int 0
 .text
 .global main
 main:
-    movl $2, %r8d
-    movl %r8d, %r9d
-    imul %r8d, %r9d
-    movl %r9d, %r10d
-    addl %r8d, %r10d
+    movl x(%rip), %r8d
+    movl %r8d, y(%rip)
+    imul x(%rip), %r8d
+    movl %r8d, y(%rip)
+    movl y(%rip), %r10d
+    addl x(%rip), %r10d
 
     movl %r10d, %esi
     movl $0, %eax
